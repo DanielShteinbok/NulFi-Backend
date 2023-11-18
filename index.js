3ce8b10ac3a31e7be7ae8bfa44caa8550244515a
Daniel Shteinbok <dshteinbok@gmail.com>
2023-11-18 17:45:21 -0500

    lots of back-and-forth, but mostly unchanged

--- index.js
+++ index.js
@@ -1,22 +1,28 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+//import express from 'express';
+//import {Client} from 'pg';
+const pg = require('pg');
 
-app.use(express.json())
+const app = express();
+const port = 3000;
+
+const pool = new pg.Pool();
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
   console.log("got a request");
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
-app.post('/', (req, res) => {
-  console.log("Received a post request!")
-  console.log(req.body)
+app.post('/', async (req, res) => {
+  console.log("Received a post request!");
+  console.log(req.body);
   // check address from signedTransaction
   // check transaction against what is allowed in the database
   // add nonce to database
-})
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
